9ed308a9e72484070d3d48bea313f8ca25257196
adeebachurail <adeebaarif1322@gmail.com>
2024-07-07 11:27:07 +0500

    intial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+// table print kre 
+
+// var userinput = +prompt("Please Enter your number :")
+
+// for (var i = userinput; i <= 10; i++){
+//         console.log(` ${userinput} * ${i}  = ${i * userinput}`)
+    
+// }
+
+
+// array elements ka sum print kre 
+// var array = []
+// var sum = 0
+// for (var i = 0; i < 5; i++){
+//     var userinput = +prompt("Enter your numbers to push :")
+//     array.push(userinput)
+// }
+
+// for(var i = 0; i < array.length; i++){
+//     sum += array[i]
+// }
+
+// console.log({sum})
+
+
+// array ka largest number 
+// var array = []
+// var largestnumber = 0
+// for (var i = 0; i < 5; i++){
+//     var userinput = +prompt("Enter your numbers to push :")
+//     array.push(userinput)
+// }
+// for(var i = 0; i < array.length; i++){
+//     if ()
+// }
+
+
+// factorial print kre 
+
+var userinput = +prompt("Enter any number for factorial :")
+var factorial = 1
+
+if (userinput > 1){
+    factorial *= userinput
+    console.log(factorial)
+}
+
+
+
+
+
+
+
+
+
+
